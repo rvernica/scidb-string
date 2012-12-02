23f7fee66fca48374501e89dfada4377d63a876b
Timothy M. Shead <tim@shead-custom-design.com>
2012-12-01 17:25:18 -0700

    Add functions to concatenate strings and return string lengths.

--- src/string.cpp
+++ src/string.cpp
@@ -9,21 +9,35 @@
 using namespace scidb;
 using namespace boost::assign;
 
-void string_to_lower(const Value** args, Value* result, void*)
+void string_add(const Value** arguments, Value* result, void*)
 {
-    result->setString(boost::to_lower_copy(std::string(args[0]->getString())).c_str());
+  const std::string lhs = std::string(arguments[0]->getString());
+  const std::string rhs = std::string(arguments[1]->getString());
+  result->setString((lhs + rhs).c_str());
 }
 
-void string_to_upper(const Value** args, Value* result, void*)
+void string_len(const Value** arguments, Value* result, void*)
 {
-    result->setString(boost::to_upper_copy(std::string(args[0]->getString())).c_str());
+  result->setInt64(std::string(arguments[0]->getString()).size());
 }
 
-void string_trim(const Value** args, Value* result, void*)
+void string_to_lower(const Value** arguments, Value* result, void*)
 {
-    result->setString(boost::trim_copy(std::string(args[0]->getString())).c_str());
+  result->setString(boost::to_lower_copy(std::string(arguments[0]->getString())).c_str());
 }
 
+void string_to_upper(const Value** arguments, Value* result, void*)
+{
+  result->setString(boost::to_upper_copy(std::string(arguments[0]->getString())).c_str());
+}
+
+void string_trim(const Value** arguments, Value* result, void*)
+{
+  result->setString(boost::trim_copy(std::string(arguments[0]->getString())).c_str());
+}
+
+REGISTER_FUNCTION(+, list_of(TID_STRING)(TID_STRING), TID_STRING, string_add);
+REGISTER_FUNCTION(len, list_of(TID_STRING), TID_INT64, string_len);
 REGISTER_FUNCTION(to_lower, list_of(TID_STRING), TID_STRING, string_to_lower);
 REGISTER_FUNCTION(to_upper, list_of(TID_STRING), TID_STRING, string_to_upper);
 REGISTER_FUNCTION(trim, list_of(TID_STRING), TID_STRING, string_trim);
