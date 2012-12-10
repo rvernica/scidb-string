974f3018b4049e91e2206616df2b55de52bdedeb
Timothy M. Shead <tim@shead-custom-design.com>
2012-12-09 20:39:59 -0700

    Use consistent function names.

--- cmake/FindScidb.cmake
+++ cmake/FindScidb.cmake
@@ -17,7 +17,7 @@
 # You may obtain a copy of the License at
 #
 #     http://www.apache.org/licenses/LICENSE-2.0
-#   
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -27,24 +27,24 @@
 include(FindPackageHandleStandardArgs)
 
 # Look for includes and libraries
-find_path(SCIDB_INCLUDE_ARRAY  array/Array.h 
+find_path(SCIDB_INCLUDE_ARRAY  array/Array.h
   HINTS ${SCIDB_ROOT_DIR}
   PATH_SUFFIXES include src
   DOC "Path to include directory for array/Array.h in Scidb source tree")
 
-find_path(SCIDB_SRC_DIR array/Metadata.cpp 
+find_path(SCIDB_SRC_DIR array/Metadata.cpp
   HINTS ${SCIDB_ROOT_DIR}
   PATH_SUFFIXES src)
 
-find_package_handle_standard_args(SCIDB DEFAULT_MSG 
-  SCIDB_INCLUDE_ARRAY 
+find_package_handle_standard_args(SCIDB DEFAULT_MSG
+  SCIDB_INCLUDE_ARRAY
   SCIDB_SRC_DIR)
 
 if(SCIDB_FOUND)
-  set(SCIDB_INCLUDES ${SCIDB_INCLUDE_ARRAY}) 
+  set(SCIDB_INCLUDES ${SCIDB_INCLUDE_ARRAY})
   set(SCIDB_CXX_FLAGS "-DPROJECT_ROOT=\\\"${SCIDB_ROOT_DIR}/\\\"")
-
 endif(SCIDB_FOUND)
+
 mark_as_advanced(Scidb_FOUND SCIDB_INCLUDES SCIDB_SRC_DIR SCIDB_CXX_FLAGS)
 
 
--- src/string.cpp
+++ src/string.cpp
@@ -9,36 +9,30 @@
 using namespace scidb;
 using namespace boost::assign;
 
-void string_add(const Value** arguments, Value* result, void*)
+void strcat(const Value** arguments, Value* result, void*)
 {
   const std::string lhs = std::string(arguments[0]->getString());
   const std::string rhs = std::string(arguments[1]->getString());
   result->setString((lhs + rhs).c_str());
 }
 
-void string_len(const Value** arguments, Value* result, void*)
-{
-  result->setInt64(std::string(arguments[0]->getString()).size());
-}
-
-void string_to_lower(const Value** arguments, Value* result, void*)
+void strlower(const Value** arguments, Value* result, void*)
 {
   result->setString(boost::to_lower_copy(std::string(arguments[0]->getString())).c_str());
 }
 
-void string_to_upper(const Value** arguments, Value* result, void*)
+void strupper(const Value** arguments, Value* result, void*)
 {
   result->setString(boost::to_upper_copy(std::string(arguments[0]->getString())).c_str());
 }
 
-void string_trim(const Value** arguments, Value* result, void*)
+void strtrim(const Value** arguments, Value* result, void*)
 {
   result->setString(boost::trim_copy(std::string(arguments[0]->getString())).c_str());
 }
 
-REGISTER_FUNCTION(+, list_of(TID_STRING)(TID_STRING), TID_STRING, string_add);
-REGISTER_FUNCTION(len, list_of(TID_STRING), TID_INT64, string_len);
-REGISTER_FUNCTION(to_lower, list_of(TID_STRING), TID_STRING, string_to_lower);
-REGISTER_FUNCTION(to_upper, list_of(TID_STRING), TID_STRING, string_to_upper);
-REGISTER_FUNCTION(trim, list_of(TID_STRING), TID_STRING, string_trim);
+REGISTER_FUNCTION(+, list_of(TID_STRING)(TID_STRING), TID_STRING, strcat);
+REGISTER_FUNCTION(strlower, list_of(TID_STRING), TID_STRING, strlower);
+REGISTER_FUNCTION(strupper, list_of(TID_STRING), TID_STRING, strupper);
+REGISTER_FUNCTION(strtrim, list_of(TID_STRING), TID_STRING, strtrim);
 
